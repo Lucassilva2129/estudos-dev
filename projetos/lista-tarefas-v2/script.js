9e7af1a4d062fd9657834047ca64812b680c0710
Lucassilva2129 <lucastesilva21@gmail.com>
2026-06-17 19:59:05 -0300

    adicionando contarTarefas

--- projetos/lista-tarefas-v2/script.js
+++ projetos/lista-tarefas-v2/script.js
@@ -8,12 +8,17 @@ function adicionarTarefas(tarefa) {
 // Função 2 — LISTAR (usa o for...of)
 function listarTarefas() {
     for (let tarefa of tarefas) {
-        console.log(tarefa) 
+        console.log(tarefa)
     }
 }
-//funçao 3 - Remover tarefas
+//Funçao 3 - Remover tarefas
 function removeTarefas(posicao) {
     tarefas.splice(posicao, 1) // Remove apenas 1 posição
+}
+
+// Contar tarefas
+function contarTarefas() {
+    console.log(tarefas.length)
 }
 
 // Chamada - adiciona as tarefas
@@ -21,5 +26,6 @@ adicionarTarefas('Estudar')
 adicionarTarefas('Fazer exercício')
 adicionarTarefas('Revisar')
 
+contarTarefas()
 removeTarefas(1) // Remove a segunda tarefa
 listarTarefas() // Lista depois de removido
