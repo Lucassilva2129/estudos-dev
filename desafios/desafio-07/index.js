0aca7f438018868d387e7a7940edf37d1e0d9c84
Lucassilva2129 <lucastesilva21@gmail.com>
2026-06-19 20:58:23 -0300

    metodo map calculando precos por peso

--- desafios/desafio-07/index.js
+++ desafios/desafio-07/index.js
@@ -1,8 +1,8 @@
 const produtos = [
     { material: 'PLA', peso: 200, horas: 4 },
     { material: 'ABS', peso: 350, horas: 6 },
-    { material: 'PETG', peso: 150, horas: 3 },            // objetos da array
+    { material: 'PETG', peso: 150, horas: 3 },            // Objetos da array
     { material: 'PLA', peso: 100, horas: 2 },
 ]
-const precos = produtos.map((item) => item.peso * 0.8)    // calcula o valor de cada item definindo um valor por gramas do peso
-console.log(precos)                                       // exibe a lista de valores de cada material
+const precos = produtos.map((item) => item.peso * 0.8)    // Cada produto teve seu preço calculado baseado no peso
+console.log(precos)                                       // Exibe a lista de valores de cada material
