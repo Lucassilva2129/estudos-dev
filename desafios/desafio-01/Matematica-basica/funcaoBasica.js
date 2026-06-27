eca1412cd382a259f439539a351de17bbc0bb2cc
Lucas <45496933+Lucassilva2129@users.noreply.github.com>
2026-06-27 15:23:54 -0300

    Funções simples somar e subtrair

--- desafios/desafio-01/Matematica-basica/funcaoBasica.js
+++ desafios/desafio-01/Matematica-basica/funcaoBasica.js
@@ -1,19 +1,12 @@
-/*const num1 = 4
-const num2 = 2
-const total = num1 + num2
-console.log(total)*/
-
-// usando funcão function 
-
-function somar(a , b){
+ function somar(a , b){
     return a + b
 }
 console.log(somar(4,2))
 console.log(somar(8,2))
 console.log(somar(10,8))
 
-function subtrair(a , b){
-    return a - b
+function subtrair(c , d){
+    return c - d
 }
 console.log(subtrair(3,2))
 console.log(subtrair(4,9))
