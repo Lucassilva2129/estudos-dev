6a07af5269d424b95b4d07f5d18eb078f5902d2c
Lucas <45496933+Lucassilva2129@users.noreply.github.com>
2026-07-02 22:51:34 -0300

    Adicionando exercicio de filter com palavras

--- desafios/desafio-11/index.js
+++ desafios/desafio-11/index.js
@@ -3,3 +3,9 @@ const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const numeros = lista.filter( numero => numero  % 2 === 0 ) // filtar os numeros divisivel por dois e resto zero ( pares ) e mostrar em uma nova lista numeros.
 
 console.log(numeros) // Lista com o filtro ( Pares )
+
+const palavras = ['sol', 'casa', 'programador', 'dev', 'javascript', 'css']
+
+const grandes = palavras.filter (palavra => palavra.length > 4) 
+
+console.log(grandes)
