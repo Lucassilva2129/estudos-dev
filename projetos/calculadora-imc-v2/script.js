cfcc0d7179a118a614b9b6dfe0945d1aa243da2b
Lucassilva2129 <lucastesilva21@gmail.com>
2026-07-10 20:03:46 -0300

    calculador imc versao 2 com array e .find()

---
+++ projetos/calculadora-imc-v2/script.js
@@ -0,0 +1,31 @@
+const altura = document.querySelector('#altura')
+const peso = document.querySelector('#peso')
+const botao = document.querySelector('#botao')
+const resultado = document.querySelector('#resultado')
+
+
+
+botao.addEventListener('click', function () {
+    const alturaValor = Number(altura.value) / 100 // converte o valor de cm para metros
+    const pesoValor = Number(peso.value)
+
+    if (alturaValor === 0 || pesoValor === 0) {
+        resultado.innerText = 'Preecha todos os campos!'
+        return
+    }
+
+    const imc = pesoValor / (alturaValor ** 2)
+
+    // Classificação do IMC
+
+    const classific = [
+        { limite: 18.5, texto: 'Abaixo do peso' },
+        { limite: 25, texto: 'Peso normal' },
+        { limite: 30, texto: 'Sobrepeso' },
+        { limite: Infinity, texto: 'Obesidade' }
+    ]
+
+    const classificado = classific.find( item => imc < item.limite).texto
+
+    resultado.innerText = `Seu IMC é ${imc.toFixed(2)}. ${classificado}`
+})
