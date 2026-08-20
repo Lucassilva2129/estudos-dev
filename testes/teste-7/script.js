e5ff3eabc09c0861d851cde6810433cae91fbcbe
Lucassilva2129 <lucastesilva21@gmail.com>
2026-08-20 19:21:50 -0300

    calculadora 3d CSS estilizacao completa

--- testes/teste-7/script.js
+++ testes/teste-7/script.js
@@ -4,14 +4,14 @@ const nome = document.querySelector('#nome')
 const email = document.querySelector('#email')
 
 botao.addEventListener('click', function() {
-    if(formulario.style.display === 'none') {
-        formulario.style.display = 'block'
-        botao.innerText = 'Fechar detalhes'
-        document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+    if(formulario.style.display === 'none') { // none oculta o formulário
+        formulario.style.display = 'block' // block exibe o formulário
+        botao.innerText = 'Fechar detalhes' // muda o texto do botão para "Fechar detalhes"
+        document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)' // muda a cor de fundo do body para um tom de cinza escuro com opacidade
     }else {
-        formulario.style.display = 'none'
+        formulario.style.display = 'none'// oculta o formulário
         botao.innerText = 'Ver detalhes'
-        document.body.style.backgroundColor = 'transparent'
+        document.body.style.backgroundColor = 'transparent' // muda a cor de fundo do body para transparente
     }
     
 })
